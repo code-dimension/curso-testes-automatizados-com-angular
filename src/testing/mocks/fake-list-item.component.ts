405bf9a27b6d1ae13566474cd1bb7ab8121774a0
Henrique Custódia <martinshenrique8@gmail.com>
2024-08-18 17:31:43 -0300

    test(ListComponent): verifica se a tarefa está sendo enviada para ListItemComponent corretamente

---
+++ src/testing/mocks/fake-list-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { ListItemComponent } from 'src/app/pages/list/list-item/list-item.component';
+import { Task } from 'src/app/shared/interfaces/task.interface';
+
+@Component({
+  selector: 'app-list-item',
+  standalone: true,
+  template: '',
+})
+export class FakeListItemComponent implements ListItemComponent {
+  
+  task = input.required<Task>();
+
+}
