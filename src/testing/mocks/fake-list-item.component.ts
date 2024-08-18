1fcd5494616a1cb81ca38f8f88a4285231416a76
Henrique Custódia <martinshenrique8@gmail.com>
2024-08-18 18:24:51 -0300

    test(ListItemComponent): implementa testes  com setup function

--- src/testing/mocks/fake-list-item.component.ts
+++ src/testing/mocks/fake-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output, OutputEmitterRef } from '@angular/core';
 import { ListItemComponent } from 'src/app/pages/list/list-item/list-item.component';
 import { Task } from 'src/app/shared/interfaces/task.interface';
 
@@ -8,7 +8,10 @@ import { Task } from 'src/app/shared/interfaces/task.interface';
   template: '',
 })
 export class FakeListItemComponent implements ListItemComponent {
-  
-  task = input.required<Task>();
+  complete = output<Task>();
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onComplete(): void {}
+
+  task = input.required<Task>();
 }
