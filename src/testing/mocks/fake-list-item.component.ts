7a483dd80aa66142758e4d061bc7f228a4bdea6e
Henrique Custódia <martinshenrique8@gmail.com>
2024-08-23 09:32:13 -0300

    test(FakeListItemComponent): atualiza mock

--- src/testing/mocks/fake-list-item.component.ts
+++ src/testing/mocks/fake-list-item.component.ts
@@ -8,14 +8,19 @@ import { Task } from 'src/app/shared/interfaces/task.interface';
   template: '',
 })
 export class FakeListItemComponent implements ListItemComponent {
+  
   task = input.required<Task>();
-
+  
   complete = output<Task>();
   notComplete = output<Task>();
+  remove = output<Task>();
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   onComplete(): void {}
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   onMarkAsPending(): void {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onRemove(): void {}
 }
