20402047a16ea6d8980df3a5d8fe10dbe3b964bd
Henrique Custódia <martinshenrique8@gmail.com>
2024-08-26 08:31:33 -0300

    feat: adiciona roteamento para CreateTaskComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-    {
-        path: '',
-        loadComponent: () => import('./pages/list/list.component').then(m => m.ListComponent),
-    }
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/list/list.component').then((m) => m.ListComponent),
+  },
+  {
+    path: 'create',
+    loadComponent: () =>
+      import('./pages/create-task/create-task.component').then(
+        (m) => m.CreateTaskComponent
+      ),
+  },
 ];
