1ff43e20e282931655f6cde6d11e15257eda71f6
Henrique Custódia <martinshenrique8@gmail.com>
2024-09-03 08:15:14 -0300

    test(TestHelper): adiciona métodos utilitários

--- src/testing/helpers/test-helper.ts
+++ src/testing/helpers/test-helper.ts
@@ -3,15 +3,43 @@ import { By } from '@angular/platform-browser';
 
 export class TestHelper<T> {
   constructor(private fixture: ComponentFixture<T>) {}
-  
+
   getTextContentByTestId(testId: string) {
     return this.queryByTestId(testId).nativeElement.textContent.trim();
   }
-  
+
   click(testId: string) {
     this.queryByTestId(testId).nativeElement.click();
   }
-  
+
+  getInputValue(testId: string) {
+    return this.queryByTestId(testId).nativeElement.value;
+  }
+
+  isCheckboxChecked(testId: string) {
+    return this.queryByTestId(testId).nativeElement.checked;
+  }
+
+  triggerInputEvent(testId: string, value: string) {
+    this.queryByTestId(testId).triggerEventHandler('input', {
+      target: {
+        value,
+      },
+    });
+  }
+
+  changeCheckbox(testId: string, value: boolean) {
+    this.queryByTestId(testId).triggerEventHandler('change', {
+      target: {
+        checked: value,
+      },
+    });
+  }
+
+  submitForm(testId: string) {
+    this.queryByTestId(testId).triggerEventHandler('submit', null);
+  }
+
   queryByTestId(testId: string) {
     return this.fixture.debugElement.query(By.css(`[data-testid="${testId}"]`));
   }
