3c0f76c33b9f108b44ca1ef8cdfbda5611cf1fcd
Henrique Custódia <martinshenrique8@gmail.com>
2024-09-10 16:05:17 -0300

    test(getTaskByIdResolver): testa cenário onde a chamada http retorna um erro 404

--- src/app/shared/resolvers/get-task-by-id/get-task-by-id.resolver.spec.ts
+++ src/app/shared/resolvers/get-task-by-id/get-task-by-id.resolver.spec.ts
@@ -14,9 +14,10 @@ import { getTaskByIdResolver } from './get-task-by-id.resolver';
 import { MockProvider } from 'ng-mocks';
 import { TasksService } from '../../services/tasks/tasks.service';
 import { Task } from '../../interfaces/task.interface';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { Component, input } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-test',
@@ -43,7 +44,7 @@ describe('getTaskByIdResolver', () => {
             {
               path: 'test/:id',
               resolve: { task: getTaskByIdResolver },
-              component: FakeComponent
+              component: FakeComponent,
             },
           ],
           withComponentInputBinding()
@@ -59,12 +60,35 @@ describe('getTaskByIdResolver', () => {
 
     (tasksService.getById as jest.Mock).mockReturnValue(of(fakeTask));
 
-    const harness =  await RouterTestingHarness.create(`test/${fakeTask.id}`);
+    const harness = await RouterTestingHarness.create(`test/${fakeTask.id}`);
 
-    const fakeComponentDebugEl = harness.fixture.debugElement.query(By.directive(FakeComponent));
+    const fakeComponentDebugEl = harness.fixture.debugElement.query(
+      By.directive(FakeComponent)
+    );
 
     expect(tasksService.getById).toHaveBeenCalledWith(fakeTask.id);
 
     expect(fakeComponentDebugEl.componentInstance.task()).toBe(fakeTask);
+  });
+
+  it('deve retornar um erro caso a tarefa não exista', async () => {
+    const tasksService = TestBed.inject(TasksService);
+
+    const fakeTask: Task = { id: '1', title: 'Item 1', completed: false };
+
+    const fakeHttpErrorResponse = new HttpErrorResponse({
+      status: 404,
+      statusText: 'Not Found',
+    });
+
+    (tasksService.getById as jest.Mock).mockReturnValue(
+      throwError(() => fakeHttpErrorResponse)
+    );
+
+    try {
+      await RouterTestingHarness.create(`test/${fakeTask.id}`);
+    } catch (error) {
+      expect(error).toEqual(fakeHttpErrorResponse);
+    }
   });
 });
