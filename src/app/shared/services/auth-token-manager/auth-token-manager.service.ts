ee7283444f168b47e7a8655191a9c479c9a12635
Henrique Custódia <martinshenrique8@gmail.com>
2024-09-17 16:05:24 -0300

    feat(AuthTokenManagerService): cria serviço com o método setItem

---
+++ src/app/shared/services/auth-token-manager/auth-token-manager.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { LocalStorageToken } from '../../tokens/local-storage.token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthTokenManagerService {
+
+  #tokenKey = 'auth-token';
+
+  localStorage = inject(LocalStorageToken);
+
+  setToken(token: string) {
+    this.localStorage.setItem(this.#tokenKey, token);
+  }
+}
