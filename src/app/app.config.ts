d7170e5ff3b944a4139dceeedb00748348d1a7f7
Henrique Custódia <martinshenrique8@gmail.com>
2024-09-20 09:41:54 -0300

    feat(appConfig): registrar setAsLoggedInIfStorageTokenExistsInitializerProvider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { addAuthorizationHeaderInterceptor } from './core/interceptors/add-authorization-header/add-authorization-header.interceptor';
+import { setAsLoggedInIfStorageTokenExistsInitializerProvider } from './core/initializers/set-as-logged-in-if-storage-token-exists/set-as-logged-in-if-storage-token-exists.initializer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes, withComponentInputBinding()),
     provideHttpClient(
       withInterceptors([addAuthorizationHeaderInterceptor])
-    )
+    ),
+    setAsLoggedInIfStorageTokenExistsInitializerProvider
   ],
 };
