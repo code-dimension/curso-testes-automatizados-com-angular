8bd868b4f8fed8157994ae1b8034edecb4623459
Henrique Custódia <martinshenrique8@gmail.com>
2024-09-27 09:13:15 -0300

    feat(AuthTokenManagerService): testa método removeToken

--- src/app/shared/services/auth-token-manager/auth-token-manager.service.spec.ts
+++ src/app/shared/services/auth-token-manager/auth-token-manager.service.spec.ts
@@ -14,6 +14,7 @@ describe('AuthTokenManagerService', () => {
         MockProvider(LocalStorageToken, {
           setItem: jest.fn(),
           getItem: jest.fn(),
+          removeItem: jest.fn(),
         }),
       ],
     });
@@ -40,5 +41,11 @@ describe('AuthTokenManagerService', () => {
     expect(storage.getItem).toHaveBeenCalledWith('auth-token');
 
     expect(result).toBe(fakeToken);
+  });
+
+  it('deve remover o token do local storage', () => {
+    service.removeToken();
+
+    expect(storage.removeItem).toHaveBeenCalledWith('auth-token');
   });
 });
